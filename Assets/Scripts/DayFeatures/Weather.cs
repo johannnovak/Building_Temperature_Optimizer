7b637cf5dfaf51366cbf1fa94c0e7f6dc24dd3aa
Johann NOVAK <johann.novak25@gmail.com>
2015-11-06 21:17:04 +0600

    json parsing OK - Weather object filled - main menu UI proto created

--- Assets/Scripts/DayFeatures/Weather.cs
+++ Assets/Scripts/DayFeatures/Weather.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Weather : MonoBehaviour {
 	
@@ -9,9 +10,64 @@ public class Weather : MonoBehaviour {
 	private float m_sunSetHour;
 	private float m_sunSetMinute;
 
-	public Weather()
+	private Dictionary<float, float> m_timeAndTemperature;
+
+	public Weather(float _sunRiseHour, float _sunRiseMinute, float _sunSetHour, float _sunSetMinute, Dictionary<float, float> _timeAndTemperature)
 	{
+		m_sunRiseHour = _sunRiseHour;
+		m_sunRiseMinute = _sunRiseMinute;
+		m_sunSetHour = _sunSetHour;
+		m_sunSetMinute = _sunSetMinute;
+		m_timeAndTemperature = _timeAndTemperature;
+	}
+	
+	public float GetSunRiseHour()
+	{
+		return m_sunRiseHour;
+	}
+	
+	public float GetSunRiseMinute()
+	{
+		return m_sunRiseMinute;
 	}
 
+	public float GetSunSetHour()
+	{
+		return m_sunSetHour;
+	}
 
+	public float GetSunSetMinute()
+	{
+		return m_sunSetMinute;
+	}
+
+	public float GetTemperatureWithTime(float _time)
+	{
+		float temperature;
+		m_timeAndTemperature.TryGetValue (_time, out temperature);
+		return temperature;
+	}
+
+	public float GetTimeAssociatedWithTemperature(float _temperature)
+	{
+		foreach(float time in m_timeAndTemperature.Keys)
+		{
+			float temperature;
+			m_timeAndTemperature.TryGetValue(time, out temperature);
+			if(temperature == _temperature)
+				return time;
+		}
+		return -1;
+	}
+
+	public override string ToString ()
+	{
+		string display = "";
+		display += "\nWeather";
+		display += "\nSunrise : " + (m_sunRiseHour < 10 ? "0" : "") + m_sunRiseHour + ":" + (m_sunRiseMinute < 10 ? "0" : "") + m_sunRiseMinute;
+		display += "\nSunset : " + (m_sunSetHour < 10 ? "0" : "") + m_sunSetHour + ":" + (m_sunSetMinute < 10 ? "0" : "") + m_sunSetMinute;
+		foreach (KeyValuePair<float, float> pair in m_timeAndTemperature)
+			display += "\nHour : " + pair.Key + "/ Temperature : " + pair.Value;
+		return display;
+	}
 }
