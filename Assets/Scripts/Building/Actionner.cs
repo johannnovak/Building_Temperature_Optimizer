763c99059398c36ce5f173a48f8f9f2a6fefaf19
Johann NOVAK <johann.novak25@gmail.com>
2015-12-21 23:35:58 +0100

    Actionner modified with cost - empty room containers created for two upper floors - TODO : walls for F1 and F2

--- Assets/Scripts/Building/Actionner.cs
+++ Assets/Scripts/Building/Actionner.cs
@@ -1,5 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Reflection;
+
+class ActionnerPropertiesAttr: Attribute
+{
+	internal ActionnerPropertiesAttr(float _wattPerHourCost)
+	{
+		WattPerHourCost = _wattPerHourCost;
+	}
+	
+	public float WattPerHourCost { get; private set;}
+}
+
+public enum ActionnerProperties 
+{
+	[ActionnerPropertiesAttr(1.05F)] RadiativeHeater = 0,
+	[ActionnerPropertiesAttr(5.5F)] GazHeater = 1,
+	[ActionnerPropertiesAttr(3.8F)] AirConditionner = 2
+}
 
 public class Actionner : MonoBehaviour {
 
@@ -8,10 +27,22 @@ public class Actionner : MonoBehaviour {
 
 	public bool Prepared { get; set;}
 
+	public ActionnerProperties m_properties;
+
 	private void Start()
 	{
 		MinDeliveredEnergy = float.NaN;
 		MaxDeliveredEnergy = float.NaN;
 		Prepared = false;
+	}	
+	
+	private static ActionnerPropertiesAttr GetAttr(ActionnerProperties _acProps)
+	{
+		return (ActionnerPropertiesAttr)Attribute.GetCustomAttribute(ForValue(_acProps), typeof(ActionnerPropertiesAttr));
+	}
+	
+	private static MemberInfo ForValue(ActionnerProperties _acProps)
+	{
+		return typeof(ActionnerProperties).GetField(Enum.GetName(typeof(ActionnerProperties), _acProps));
 	}
 }
